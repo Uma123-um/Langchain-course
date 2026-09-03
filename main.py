370a055fdf983543a9e6e373c98fa285b360a842
uvcodes2122 <uvcodes2122@gmail.com>
2026-09-04 00:12:29 +0530

    update search-agent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+from typing import List
+from pydantic import BaseModel,Field
 
 load_dotenv()
 from langchain.agents import create_agent
@@ -9,33 +11,34 @@ from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 import os
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
 
-tavily_search = TavilySearch(max_results=5)
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response with answer and sources"""
+
+    answer : str = Field(description="The agent's answer to the query")
+    sources : List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
 
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query : The query to search for
 
-    Returns:
-        The search results as a string
 
-    """
-    print(f"Searching for : {query}")
-    return str(tavily_search.invoke({"query": query}))
+
+
+
 
 llm = ChatAzureOpenAI(azure_deployment=os.environ.get("AZURE_DEPLOYMENT_NAME"),
     azure_endpoint=os.environ.get("AZURE_ENDPOINT"),
     api_key=os.environ.get("AZURE_API_KEY"),
     api_version="2024-02-01",
-    temperature=0.3,)
+    temperature=0.3)
 #llm = ChatOllama(model="gemma3:270m", temperature=0.3)
 
-tools = [search]
-agent = create_agent(model=llm, tools=tools)
+tools = [TavilySearch(max_results=5)]
+agent = create_agent(model=llm, tools=tools,response_format=AgentResponse)
+
 
 
 def main():
